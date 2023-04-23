0fbe48557d9b312f8f738b0220353cb5e2deacce
salman.farsi <salman.farsi@3together.us>
2023-04-23 18:43:29 -0400

    RabbitMQPractice Projects

---
+++ Fist_RabbitMQ_App/Consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    byte[] body = ea.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(" [x] Received {0}", message);
+};
+
+channel.BasicConsume(
+    queue: "letterbox",
+    autoAck: true,
+    consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ Fist_RabbitMQ_App/Producer/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "letterbox",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+while (true)
+{
+    Console.WriteLine("Enter a message to send to the letterbox:");
+    var message = Console.ReadLine();
+    
+    if (string.IsNullOrWhiteSpace(message) || message.ToLower() == "exit")
+    {
+        break;
+    }
+    
+    var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "",
+                         routingKey: "letterbox",
+                         basicProperties: null,
+                         body: encodedMessage);
+    Console.WriteLine(" [x] Sent {0}", message);
+}
